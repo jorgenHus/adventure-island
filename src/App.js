acaab3beb059a3ff0c332cd7296c4e54dde81e78
Jørgen <jorghus@gmail.com>
2022-12-27 14:51:30 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import pic from './img/story1-1.jpg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Eventyrøya</h1>
+      <img src={pic} alt="text"/>
     </div>
   );
 }
